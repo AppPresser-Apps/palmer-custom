45c0b1cf05b13117e6e4679c1a85605310e65b1e
modemlooper <modemlooper@gmail.com>
2024-04-13 13:20:50 -0700

    Refactor hero slider CSS class name for consistency

--- blocks/build/hero-slider/index.asset.php
+++ blocks/build/hero-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'e1c42991fc1d4966e4ed');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '05d74112d7e3f1467fd2');
